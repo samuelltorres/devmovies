5d7eb8bc789ce2d1d3dd7a981cdc30092e397b9b
samuelltorres <samuelltorres145@gmail.com>
2023-09-21 14:40:42 -0300

    :boom: fix: container do slider

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.button`
+export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 
-	background: #111;
+	background: inherit;
 	border: none;
 
 	img {
