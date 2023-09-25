509b55d4c47c9aa0c4f508cf444057980204c6a3
samuelltorres <samuelltorres145@gmail.com>
2023-09-25 18:28:42 -0300

    :sparkles: feat: Tela de filmes

--- src/containers/Movies/index.jsx
+++ src/containers/Movies/index.jsx
@@ -1,10 +1,38 @@
-function Movies() {
+import { useState, useEffect } from 'react';
+import { Container } from './styles';
+import { getTopMovies } from '../../services/getData';
+import { getImages } from '../../services/utils/getImages';
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+	const [topMovies, setTopMovies] = useState();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		async function getAllData() {
+			Promise.all([getTopMovies()])
+				.then(([topMovies]) => {
+					setTopMovies(topMovies);
+				})
+				.catch((error) => console.error(error));
+		}
+
+		getAllData();
+	}, []);
+
 	return (
-		<div>
-			<h1>movies</h1>
-			<p>essa é a movies</p>
-		</div>
+		<Container>
+			{topMovies &&
+				topMovies.map((movie) => (
+					<img
+						key={movie.id}
+						src={getImages(movie.poster_path || movie.profile_path || '')}
+						onClick={() => navigate(`/detalhe/${movie.id}`)}
+						alt=""
+					/>
+				))}
+		</Container>
 	);
 }
 
-export default Movies;
+export default Home;
